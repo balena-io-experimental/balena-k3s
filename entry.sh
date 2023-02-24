aeabfad2fa111e8ad38d417ace6dff7ff6d24728
Kyle Harding <kyle@balena.io>
2023-02-24 13:12:32 -0500

    Add additional tls san
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- entry.sh
+++ entry.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-mkdir -p /config
-
-K3S_HOST="$(echo "${K3S_URL}" | awk -F'[/:]' '{print $4}' | xargs dig +short)"
-
-sed -r "s|server: .+$|server: https://${K3S_HOST}:6443|" /output/kubeconfig.yaml > /config/kubeconfig.yaml
-
 kubectl apply -f /kubernetes --recursive
 
 tail -f /dev/null
