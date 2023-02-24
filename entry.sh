4bbb1ed78be78f6a7f21b44ec0e598f983d76157
Kyle Harding <kyle@balena.io>
2023-02-24 09:01:15 -0500

    Adjust kubeconfig to use K3S_URL
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- entry.sh
+++ entry.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+mkdir -p /config
+
+sed "s|server: .+$|server: ${K3S_URL}|" /output/kubeconfig.yaml > /config/kubeconfig.yaml
+
 kubectl apply -f /kubernetes --recursive
 
 tail -f /dev/null
