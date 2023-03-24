dd6ee50eb4494e32f219b0fe648518d1fa798aa1
Kyle Harding <kyle@balena.io>
2023-03-24 09:15:51 -0400

    Resolve environment variables in kubernetes files
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- entry.sh
+++ entry.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-# TODO: recursively run envsubst on all files in /kubernetes
+set -x
+
+# recursively run envsubst on all files in /kubernetes
+find /kubernetes -type f -name "*.yml" -exec sh -c 'envsubst < "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
 
 kubectl apply -f /kubernetes --recursive
 
