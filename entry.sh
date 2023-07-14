31dec09b2865b5aec685775ee05cff1bd945739f
Carlo Miguel F. Cruz <carloc@balena.io>
2023-07-14 11:52:30 +0800

    Image pull secrets should be defined as kubernetes manifests.
    The feature for automatic creation of an image pull secret for
    docker.io has been removed.
    
    Image pull secrets should now be defined as a kubernetes manifest
    included in the kustomization.  The instructions in the README
    have been updated to show how to add an image pull secret
    for Dockerhub.
    
    Change-type: minor
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- entry.sh
+++ entry.sh
@@ -2,19 +2,9 @@
 
 set -euo pipefail
 
-kubectl delete secret image-pull-secret 2>/dev/null || true
-
-if [ -n "${DOCKER_USERNAME:-}" ] && [ -n "${DOCKER_PASSWORD:-}" ]; then
-	echo "Creating DockerHub image-pull-secret from environment variables"
-	kubectl create secret docker-registry image-pull-secret \
-		--docker-server='https://index.docker.io/v1/' \
-		--docker-username="${DOCKER_USERNAME}" \
-		--docker-password="${DOCKER_PASSWORD}"
-fi
-
 while true; do
-	# recursively run envsubst on all files in /kubernetes
-	find /kubernetes -type f -name "*.yml" -exec sh -c 'envsubst < "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
-	kubectl apply -k /kubernetes
-	sleep ${UPDATE_INTERVAL:-600}
+  # recursively run envsubst on all files in /kubernetes
+  find /kubernetes -type f -name "*.yml" -exec sh -c 'envsubst < "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
+  kubectl apply -k /kubernetes
+  sleep ${UPDATE_INTERVAL:-600}
 done
